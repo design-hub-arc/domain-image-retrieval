aa2efb5c7b0e741edab4d78b8ae0a5c084e79504
Omar Costilla <ocost6@gmail.com>
2021-05-05 14:55:36 -0700

    removed components not relevant to selenium

--- src/main.py
+++ src/main.py
@@ -1,51 +1,57 @@
-import sys, os
-from urllib.request import urlopen
-from urllib.parse import urlparse
-from bs4 import BeautifulSoup
-from getpass import getpass
-import util
-import canvas
-import arguments
+import os
+from selenium import webdriver
+from selenium.common import exceptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import test_domains as websites
+import re
 
-if __name__ == "__main__":
+# TEST using chrome
 
-    args = arguments.make_args()
-    main_url = args.url
+# Selenium browser driver setup
+driver_name = "chromedriver.exe"
+PATH = os.path.join(os.getcwd(), driver_name)
 
-    # check if credentials needed
-    if args.login:
-        print("Username: ",end="")
-        username = input()
-        password = getpass()
-        exit()
+driver = webdriver.Chrome(PATH)
+driver.get(websites.CANVAS_LOGIN)
+driver.maximize_window()
 
-    # get domain
-    domain = urlparse(main_url).netloc
+# Let user log in 
+# finish loggin in in 30 seconds, check if successful with url
+# TODO: replace with GUI confirm
+timeout = 30
+print("Please log into page within {} seconds".format(timeout))
+try:
+    wait = WebDriverWait(driver, timeout) # timeout in 30s
+    element = wait.until(EC.url_contains("login_success=1"))
+except exceptions.TimeoutException:
+    print("Error: did not log in in time")
+    driver.quit()
+    exit()
+print("Successfully logged in\n")
 
-    # make soup
-    soup = util.make_soup(main_url)
-    
-    # check if valid. If not, quit
-    if not soup:
-        print("Failure to access domain url: {}".format(main_url))
-        exit()
+# let user select course
+# TODO: replace with GUI confirm course selection
+# currently will latch onto first course selected
+print("Click on the canvas course to crawl")
+try:
+    wait = WebDriverWait(driver, 30) # timeout in 30s
+    element = wait.until(EC.url_contains("/courses/"))
+except exceptions.TimeoutException:
+    print("Error: did not choose course in time")
+    driver.quit()
+    exit()
+print("Course selected: {}\n".format(driver.title))
 
-    # get href links
-    link_tags = soup.find_all("a")
+# strip canvas unique course number
+domain_url = driver.current_url
+regex = r"(?<=lrccd.instructure.com)[^$]+"
+regex_search = re.search(regex, domain_url)
+course_number = regex_search.group(0)
+print("course tag: {}".format(course_number))
 
-    # create main output if not existing
-    main_folder_path = os.path.join(os.getcwd(), "domains")
-    if not os.path.isdir(main_folder_path):
-        os.mkdir(main_folder_path)
+# get images and links
+css_search =  '[href*="{}"]'.format(course_number)
+domain_links = driver.find_elements_by_css_selector(css_search)
 
-    # create instance folder
-    folder_name = "{}".format(domain.replace(".","-"))
-    folder_path = util.make_folder(folder_name, main_folder_path)
-
-    # write domain links to file
-    domain_urls = util.get_domain_links(link_tags, main_url)
-    util.write_to_file(domain_urls, "pages", folder_path, folder_name)
-
-    # write image links to file
-    img_urls = util.get_img_links(domain_urls, main_url) 
-    util.write_to_file(img_urls, "imgs", folder_path, folder_name)
+driver.quit()
